be4f2c5b3df152d50d1ec80d44669233b4b28357
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-24 16:18:19 +0300

    getting a cookie theme

--- src/app/api/headers/route.js
+++ src/app/api/headers/route.js
@@ -4,8 +4,11 @@ export async function GET(request){
 const requestheaders=new Headers(request.headers);
 const headerList=headers();
 
+const theme=request.cookies.get("theme")
+
 console.log(headerList.get("Authorization"))
 console.log(requestheaders.get("Authorization"))
+console.log(theme)
 
 return new Response("Request Headers Collection",{
     headers:{
