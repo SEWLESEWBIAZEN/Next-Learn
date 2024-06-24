e696049ba5686948e92d45277ed1de5613662037
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-24 16:13:43 +0300

    adding request headers

---
+++ src/app/api/headers/route.js
@@ -0,0 +1,16 @@
+import { headers } from "next/headers";
+
+export async function GET(request){
+const requestheaders=new Headers(request.headers);
+const headerList=headers();
+
+console.log(headerList.get("Authorization"))
+console.log(requestheaders.get("Authorization"))
+
+return new Response("Request Headers Collection",{
+    headers:{
+        "Content-Type":"text/html"
+    }
+})
+    
+}
